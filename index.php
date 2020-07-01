18c06e0449f4e971ffafb0324536c2b19b15c336
0xFr0zen <0xfr0zen@gmail.com>
2020-07-01 11:37:13 +0200

    updates

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
 
+include_once __DIR__.'/../essentials/header.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evop</title>
+
+</head>
+<body>
+    
+</body>
+</html>
