35745bda950267ccbe318ac6fe710d392131bc1b
0xFr0zen <0xfr0zen@gmail.com>
2020-07-01 11:40:19 +0200

    updates

--- index.php
+++ index.php
@@ -9,7 +9,13 @@ include_once __DIR__.'/../essentials/header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evop</title>
+    <?php
+    _header("",
+        array("name" => "landingpage", "type" => "js"),
+        array("name" => "main", "type" => "css")
+    );
 
+    ?>
 </head>
 <body>
     
